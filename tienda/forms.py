f33ed38a61e4a1c8acd770b36594f5daa7201c57
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-15 23:25:25 -0500

    Validar campos

--- tienda/forms.py
+++ tienda/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class RegisterForm(forms.Form):
     usuario = forms.CharField(required=True,
@@ -6,5 +7,29 @@ class RegisterForm(forms.Form):
                                 widget=forms.TextInput(attrs={'class':'form-control','id':'username','placeholder':'Usuario'}))
     correo = forms.EmailField(required=True,
                                 widget=forms.EmailInput(attrs={'class':'form-control','id':'correo','placeholder':'Correo Electronico'}))
-    password = forms.CharField(required=True,
-                                widget=forms.PasswordInput(attrs={'class':'form-control','id':'password','placeholder':'Password'}))
+    password = forms.CharField(label = 'Contraseña',
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'class':'form-control','id':'password'}))
+    password2 = forms.CharField(label = 'Confirmar Contraseña',
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'class':'form-control','id':'password2'}))
+
+    def clean_usuario(self):
+        usuario = self.cleaned_data.get('usuario')
+        if User.objects.filter(username=usuario).exists():
+            raise forms.ValidationError('El usuario no se encuentra disponible')
+        
+        return usuario
+
+    def clean_correo(self):
+        correo = self.cleaned_data.get('correo')
+        if User.objects.filter(email=correo).exists():
+            raise forms.ValidationError('El correo ya se encuentra en uso')
+        
+        return correo
+
+    def clean(self):#Siempre y cuando se necesite validar campos o atributos que dependan unos de otros
+        cleaned_data = super().clean()
+
+        if cleaned_data.get('password2') != cleaned_data.get('password'):
+            self.add_error('password2', 'La contraseña no coincide')
