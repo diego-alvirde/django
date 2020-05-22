bfd1d0326573a87778fc80adbdd8dea92a1bf718
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-22 16:03:17 -0500

    Imagenes

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 from products.views import ProductListView
 from django.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
@@ -12,3 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('productos/', include('products.urls'))
 ]
+
+#Permitira usar/mostrar las imagenes
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
