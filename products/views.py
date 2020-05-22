2eeee0a259380b82c162a1286cd5a937bc113fbd
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-21 20:36:46 -0500

    Class DetailView

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Product
 
 # Create your views here.
@@ -14,3 +15,15 @@ class ProductListView(ListView):
         print(context) 
 
         return context
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)        
+
+        print(context) 
+
+        return context
+
--- tienda/urls.py
+++ tienda/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from products.views import ProductListView
+from django.urls import include
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('usuarios/logout', views.logout_view, name='logout'),
     path('usuarios/register', views.register, name='register'),
     path('admin/', admin.site.urls),
+    path('productos/', include('products.urls'))
 ]
