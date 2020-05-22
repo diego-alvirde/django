9f9fb20492cfd54d8b49e5a2709bb80763318254
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-21 19:55:54 -0500

    Listado de Productos

--- tienda/views.py
+++ tienda/views.py
@@ -7,16 +7,14 @@ from django.contrib.auth import authenticate
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .forms import RegisterForm
+from products.models import Product
 
 def index(request):
+    products = Product.objects.all().order_by('-id')
     return render(request,"index.html", {
         'message':'Listado de Productos',
         'titulo':'Productos',
-        'products': [
-            {'title':'Playera','price':5,'stock':True},
-            {'title':'Camisa','price':7,'stock':True},
-            {'title':'Mochila','price':20,'stock':False}
-        ]
+        'products': products
     })
 
 def login_view(request):
