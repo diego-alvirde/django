0eedceee31c6a6f99f62fc703fe7c1adb2177a0a
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-27 16:46:16 -0500

    Sesiones

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart(request):
+    #Crear una sesión
+    #request.session['cart_id'] = '123'
+    #Obtener valor de una sesión
+    #valor = request.session.get('cart_id')    
+    #Eliminar una sesión
+    #request.session['cart_id'] = None
+
     return render(request, 'carts/carts.html', {
 
     })
