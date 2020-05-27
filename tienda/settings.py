e8f778a3c186eddeb4a861c685ee692c891591c3
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-27 14:55:10 -0500

    Usuarios abstractos

--- tienda/settings.py
+++ tienda/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tienda.urls'
 
+AUTH_USER_MODEL = 'users.User'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- users/models.py
+++ users/models.py
@@ -1,7 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+#Si se quiere sobreescribir el modelo User
+#AbstractBaseUser
+"""
+id
+password
+last_login
+"""
+
+#AbstractUser
+"""
+username
+first_name
+last_name
+email
+password
+groups
+user_permissions
+is_staff
+is_active
+is_superuser
+last_login
+date_joined
+"""
+
+class User(AbstractUser):
+    def get_full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
 class Customer(User):
     class Meta:
         proxy = True
